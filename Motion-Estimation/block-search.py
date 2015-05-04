449b4d916d2e4f131723a740e9973d9195076c59
zongzebing@gmail.com <zongzebing@gmail.com>
2015-05-04 02:11:30 -0700

    Implement naive full search algorithm

---
+++ Motion-Estimation/block-search.py
@@ -0,0 +1,38 @@
+import sys
+from scipy import misc
+
+# Read image
+frame1 = misc.imread("frame_1.jpg")
+frame2 = misc.imread("frame_2.jpg")
+
+# Convert uint8 to float
+frame1 = frame1.astype(float)
+frame2 = frame2.astype(float)
+(row, col) = frame1.shape
+
+# Get the target block
+block_target = frame2[64:96, 80:112]
+(a, b) = block_target.shape
+
+min_SAE = sys.float_info.max
+min_i = -1
+min_j = -1
+
+for i in range(row - a):
+	for j in range(col - b):
+		block_compare = frame1[i:i+a, j:j+b]
+		sum = 0
+		for x in range(a):
+			for y in range(b):
+				sum += abs(block_target[x][y] - block_compare[x][y])
+		if sum < min_SAE:
+			min_i = i
+			min_j = j
+			min_SAE = sum
+
+print min_i
+print min_j
+print min_SAE/32/32
+
+
+
